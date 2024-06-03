6445f643b680dcb9ce998882520eee62432e9ec4
Stephen Muriithi Munene <smuriithi7969@gmail.com>
2024-06-03 17:35:46 +0300

    submits bears lab

---
+++ lib/insert.sql
@@ -0,0 +1,8 @@
+INSERT INTO bears (name , age, sex,color ,temperament, alive) VALUES("Mr. Chocolate",21,"M","Black","angry",1);
+INSERT INTO bears (name , age, sex,color ,temperament, alive) VALUES("Rowdy", 19,"M","Silverback","oppy",2);
+INSERT INTO bears (name , age, sex,color ,temperament, alive) VALUES("Tabitha", 20,"F","White","snowbunny",1);
+INSERT INTO bears (name , age, sex,color ,temperament, alive) VALUES("Sergeant Brown",25,"F","Brown","strong",1);
+INSERT INTO bears (name , age, sex,color, temperament, alive) VALUES("Melissa", 28,"F", "Yellow","pretty",1);
+INSERT INTO bears (name , age, sex,color, temperament, alive) VALUES("Grinch",23,"M","Black","weird",2);
+INSERT INTO bears (name , age, sex,color, temperament, alive) VALUES("Wendy", 18, "F","Brown", "strong",1);
+INSERT INTO bears (name , age, sex,color, temperament, alive) VALUES(NULL, NULL, "M", "White","lost",1);
